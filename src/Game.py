b88c203a74f242f01ae3c57a8f3ed59c87fac398
lwalton101 <waltonfamily01@outlook.com>
2023-06-07 19:10:00 +0100

    removed run() from Game and added init code for pg

--- src/Game.py
+++ src/Game.py
@@ -1,15 +1,21 @@
+import pygame
+
 class Game():
 
     VERSION = 0.1
 
-    def __init__(self, screenWidth: int, screenHeight: int, frameRate: int) -> None:
+    def __init__(self, screenWidth: int, screenHeight: int, frameRate: int, title:str) -> None:
         self.screenWidth = screenWidth
         self.screenHeight = screenHeight
         self.frameRate = frameRate
-        print(f"Game made with VroomVroom version {self.VERSION}")
-        pass
+        self.title = title
 
-    def run(self) -> None:
+        pygame.init()
+        self.screen = pygame.display.set_mode((screenWidth, screenHeight))
+        pygame.display.set_caption(title)
+        self.clock = pygame.time.Clock()
+
+        print(f"Game made with VroomVroom version {self.VERSION}")
         pass
 
     def loop(self) -> None:

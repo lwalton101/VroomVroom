51f9beb089f488249abf5154038b4b7ecb5cc501
lwalton101 <waltonfamily01@outlook.com>
2023-06-07 19:04:04 +0100

    began game class

---
+++ src/Game.py
@@ -0,0 +1,12 @@
+class Game():
+    def __init__(self, screenWidth: int, screenHeight: int, frameRate: int) -> None:
+        self.screenWidth = screenWidth
+        self.screenHeight = screenHeight
+        self.frameRate = frameRate
+        pass
+
+    def run(self) -> None:
+        pass
+
+    def loop(self) -> None:
+        pass

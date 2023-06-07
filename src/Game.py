756a4569a7a91895d5cf42d652c400c49e2eed8d
lwalton101 <waltonfamily01@outlook.com>
2023-06-07 19:06:38 +0100

    added credit in print

--- src/Game.py
+++ src/Game.py
@@ -1,8 +1,12 @@
 class Game():
+
+    VERSION = 0.1
+
     def __init__(self, screenWidth: int, screenHeight: int, frameRate: int) -> None:
         self.screenWidth = screenWidth
         self.screenHeight = screenHeight
         self.frameRate = frameRate
+        print(f"Game made with VroomVroom version {self.VERSION}")
         pass
 
     def run(self) -> None:

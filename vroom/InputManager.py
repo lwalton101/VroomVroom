7f8b617e55605ea8039773c1abe33ddfd1e1949a
lwalton101 <waltonfamily01@outlook.com>
2023-06-08 12:39:31 +0100

    made InputManager static and added docs

--- vroom/InputManager.py
+++ vroom/InputManager.py
@@ -1,33 +1,65 @@
 class InputManager:
-    def __init__(self) -> None:
-        self.keysDown = []
-        self.keysPressed = []
-        self.keysUp = []
 
-    def handleKeydownEvent(self, keyIndex: int) -> None:
-        self.keysDown.append(keyIndex)
-        self.keysPressed.append(keyIndex)
+    keysDown: list[int] = []
+    keysPressed: list[int] = []
+    keysUp: list[int] = []
 
-    def handleKeyupEvent(self, keyIndex: int) -> None:
-        self.keysUp.append(keyIndex)
-        self.keysPressed.remove(keyIndex)
+    @staticmethod
+    def handleKeydownEvent(keyIndex: int) -> None:
+        """
+        The handleKeydownEvent function is called when a key is pressed.
+        It adds the keyIndex to the keysDown list and also adds it to the keysPressed list.
+        
+        :param self: Refer to the object itself
+        :param keyIndex: int: Determine which key is pressed
+        :return: None
+        :doc-author: Trelent
+        """
+        InputManager.keysDown.append(keyIndex)
+        InputManager.keysPressed.append(keyIndex)
 
-    def update(self) -> None:
-        self.keysDown = []
-        self.keysUp = []
+    @staticmethod
+    def handleKeyupEvent(keyIndex: int) -> None:
+        """
+        The handleKeyupEvent function is called when a keyup event occurs.
+        It adds the keyIndex to the keysUp list and removes it from the keysPressed list.
+        
+        :param self: Refer to the object itself
+        :param keyIndex: int: Determine which key is being pressed
+        :return: None
+        :doc-author: Trelent
+        """
+        InputManager.keysUp.append(keyIndex)
+        InputManager.keysPressed.remove(keyIndex)
 
-    def debug(self) -> None:
-        print(f"Keys Down: {self.keysDown}")
-        print(f"Keys Pressed: {self.keysPressed}")
-        print(f"Keys Up: {self.keysUp}")
+    @staticmethod
+    def update() -> None:
+        """
+        The update function is called every frame, and it updates the keysDown and keysUp lists.
+            The update function should be called at the end of every game loop.
+        
+        :return: None
+        :doc-author: Trelent
+        """
+        InputManager.keysDown = []
+        InputManager.keysUp = []
 
-    def IsKeyDown(self, key: int):
-        return key in self.keysDown
+    @staticmethod
+    def debug() -> None:
+        print(f"Keys Down: {InputManager.keysDown}")
+        print(f"Keys Pressed: {InputManager.keysPressed}")
+        print(f"Keys Up: {InputManager.keysUp}")
+
+    @staticmethod
+    def IsKeyDown(key: int):
+        return key in InputManager.keysDown
     
-    def IsKeyPressed(self, key: int):
-        return key in self.keysPressed
+    @staticmethod
+    def IsKeyPressed(key: int):
+        return key in InputManager.keysPressed
     
-    def IsKeyUp(self, key: int):
-        return key in self.keysUp
+    @staticmethod
+    def IsKeyUp(key: int):
+        return key in InputManager.keysUp
 
         
